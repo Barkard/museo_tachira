393ea99be4f148c26540378405b56eeff6723669
yerson014 <201276989+yerson014@users.noreply.github.com>
2026-01-21 22:16:53 -0400

    Agregar la vista de editar el perfil

--- resources/js/pages/Profile/EditProfile.tsx
+++ resources/js/pages/Profile/EditProfile.tsx
@@ -34,7 +34,7 @@ const EditProfile = ({ user }: Props) => {
     };
 
     return (
-        <AppLayout header="Editar Perfil">
+        <AppLayout>
             <div className="py-12 px-4 sm:px-6 lg:px-8 bg-gray-50/50 min-h-[calc(100vh-64px)]">
                 <div className="max-w-4xl mx-auto">
                     {/* Encabezado */}
